cf76cdcc709922b7a5e7764d8c00e563ee2e80ad
2011Karthik <100004622+2011Karthik@users.noreply.github.com>
2023-04-17 16:46:25 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ score=0;
 
 
 function draw {
-    check_sketch() 
+    check_sketch(); 
     if(drawn_sketch==sketch){
         answer_holder="set";
         score++;
@@ -23,10 +23,10 @@ function draw {
 }
 
 function check_sketch {
-    timer_counter++
+    timer_counter++;
     if(timer_counter>400){
         timer_counter=0;
-        timer_check="completed"
+        timer_check="completed";
     }
     if(timer_check=='completed'||answer_holder=='set') {
 timer_check="";
@@ -36,3 +36,31 @@ updateCanvas();
     
 }
 
+function preload() {
+    classifier = ml5.imageClassifier(doodlenet);
+}
+
+function setup() {
+    canvas = createCanvas(280, 280);
+    canvas.center();
+    background("white");
+    canvas.mouseReleased(classifyCanvas);
+    synth = window.speechSynthesis;
+}
+
+
+   function classifyCanvas() {
+    classifier.classify(canvas, gotResult);
+ }
+
+ function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+    }
+    console.log(results);
+    document.getElementById('Your Sketch').innerHTML = 'Your Sketch: ' + results[0].label;
+
+    document.getElementById('sketch').innerHTML = 'sketch: ' + Math.round(results[0].confidence * 100) + '%'
+    utterThis = new SpeechSynthesisUtterance(results[0].label);
+    synth.speak(utterThis);
+ }
